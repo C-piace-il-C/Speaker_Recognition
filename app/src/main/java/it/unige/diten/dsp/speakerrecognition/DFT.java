99d656ec53b0275db907f7b7e478576a01034980
Andrea Luzzati <andrea.luzzati@gmail.com>
2015-05-22 23:32:59 +0200

    Update DFT.java

--- app/src/main/java/it/unige/diten/dsp/speakerrecognition/DFT.java
+++ app/src/main/java/it/unige/diten/dsp/speakerrecognition/DFT.java
@@ -7,8 +7,11 @@ package it.unige.diten.dsp.speakerrecognition;
 
 public abstract class DFT {
 
-    public static Complex[] computeDFT(short[] src) {
-        Complex[] mDFT = new Complex[src.length];
+    public static void computeDFT(short[] src, Complex[] dest) {
+        
+        if(dest.length < src.length)
+            return;
+        
         double angle;
 
         for(int k=0; k<src.length; k++) {
@@ -17,11 +20,9 @@ public abstract class DFT {
             for (int n = 0; n < src.length; n++) {
                 angle = -2*Math.PI*(double)k*(double)n/(double)src.length;
 
-                mDFT[k].re += ((double)src[n]) * Math.cos(angle);
-                mDFT[k].im += ((double)src[n]) * Math.sin(angle);
+                dest[k].re += ((double)src[n]) * Math.cos(angle);
+                dest[k].im += ((double)src[n]) * Math.sin(angle);
             }
         }
-
-        return(mDFT);
     }
 }
