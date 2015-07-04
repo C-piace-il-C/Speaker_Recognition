715309c4de01243d2ff6738c6299657e97b2ab1a
Andrea Luzzati <andrea.luzzati@gmail.com>
2015-07-04 14:44:34 +0200

    Update DD.java

--- app/src/main/java/it/unige/diten/dsp/speakerrecognition/DD.java
+++ app/src/main/java/it/unige/diten/dsp/speakerrecognition/DD.java
@@ -7,6 +7,9 @@ public abstract class DD
 {
     public static double[][] computeDD (double[][] src, int M)
     {
+        // Per chiarezza del codice consiglio di rimpiazzare src.length
+        // e src[0].length (roba asssolutamente criptica) con FRAME_COUNT e MFCC_COUNT
+        // visto che li abbiamo...
         double ret[][] = new double[src.length][src[0].length];
 
         // Sum of first N squares: (N(N + 1)(2N + 1)) / 6.
@@ -15,13 +18,18 @@ public abstract class DD
         // f: frame index
         for (int f = 0; f < src.length; f++)
         {
-            // k: CC index
+            // k: MFCC index
             for (int k = 0; k < src[0].length; k++)
             {
                 ret[f][k] = .0;
 
                 for (int m = -M; m <= M; m++)
                 {
+                    // meglio: 
+                    // for(int n = min; n <= max; n++) invece di un if dentro ogni iterazione del for
+                    // visto che f+m+n >= 0, e quindi n >= -f -m, allora min = Math.max(-f-m,-M)
+                    // visto che f+m+n < src.length, e quindi n < src.length - m - f, allora 
+                    //      max = Math.min(src.length - m - f, M)
                     for (int n = -M; n <= M; n++)
                     {
                         if ((f + m + n >= 0) && (f + m + n < src.length))
