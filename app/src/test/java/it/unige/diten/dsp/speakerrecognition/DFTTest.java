e3e0e58e1dbef08780493a4256e1d7db4ed1bfca
Your Name <andrea.luzzati@gmail.com>
2015-07-05 13:05:42 +0200

    Finito primo test DFT con sinusoide, risultato corretto

--- app/src/test/java/it/unige/diten/dsp/speakerrecognition/DFTTest.java
+++ app/src/test/java/it/unige/diten/dsp/speakerrecognition/DFTTest.java
@@ -11,10 +11,10 @@ public class DFTTest extends TestCase {
     static double[] values = {0, 2, 4, 8, 16, 8, 4, 2, 0};
     static Complex[] results = new Complex[values.length];
 
-    public Boolean areEqual(double a, double b)
+    public boolean areEqual(double a, double b)
     {
-        double abs = (a-b < 0 ? b-a : a-b);
-        return(abs < 0.0001);
+        double abs = a-b < 0 ? (b-a) : (a-b);
+        return(abs <= 0.0001);
     }
     public void setUp() throws Exception {
         super.setUp();
@@ -46,7 +46,8 @@ public class DFTTest extends TestCase {
     }
 
 */
-    public void testDFTofSin1() throws Exception {
+    public void testDFTofSin1() throws Exception
+    {
         double[] input = new double[10];
         for(int i=0;i<input.length;i++)
             input[i] = Math.sin((double)i);
@@ -57,5 +58,23 @@ public class DFTTest extends TestCase {
         DFT.computeDFT(input, res);
 
         Complex[] expectedRes = new Complex[10];
+        int k = 0;
+        expectedRes[k++] = new Complex(1.955209482107380,0.000000000000000);
+        expectedRes[k++] = new Complex(3.151505791271589,0.594994616210640);
+        expectedRes[k++] = new Complex(-3.073479308812598,-1.118520711871261);
+        expectedRes[k++] = new Complex(-0.639028043502262,-0.304593821408143);
+        expectedRes[k++] = new Complex(-0.301436118898938,-0.118492185600151);
+        expectedRes[k++] = new Complex(-0.230334122222961,0.000000000000000);
+        expectedRes[k++] = new Complex(-0.301436118898938,0.118492185600151);
+        expectedRes[k++] = new Complex(-0.639028043502262,0.304593821408143);
+        expectedRes[k++] = new Complex(-3.073479308812598,1.118520711871261);
+        expectedRes[k++] = new Complex(3.151505791271589,-0.594994616210640);
+
+        for(int j=0;j<10;j++)
+        {
+            Assert.assertTrue(areEqual(expectedRes[j].Re, res[j].Re));
+            Assert.assertTrue(areEqual(expectedRes[j].Im, res[j].Im));
+        }
+
     }
 }
