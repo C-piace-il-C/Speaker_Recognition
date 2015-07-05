bec80b6b407fcb53d064330fdfb672185d080d39
Your Name <andrea.luzzati@gmail.com>
2015-07-05 13:15:13 +0200

    Finito primo test DFT con sinusoide, risultato corretto

--- app/src/test/java/it/unige/diten/dsp/speakerrecognition/DFTTest.java
+++ app/src/test/java/it/unige/diten/dsp/speakerrecognition/DFTTest.java
@@ -9,7 +9,6 @@ import junit.framework.TestCase;
 public class DFTTest extends TestCase {
 
     static double[] values = {0, 2, 4, 8, 16, 8, 4, 2, 0};
-    static Complex[] results = new Complex[values.length];
 
     public boolean areEqual(double a, double b)
     {
@@ -55,6 +54,7 @@ public class DFTTest extends TestCase {
         Complex[] res = new Complex[10];
         for(int i=0;i<10;i++)
             res[i] = new Complex();
+
         DFT.computeDFT(input, res);
 
         Complex[] expectedRes = new Complex[10];
@@ -68,7 +68,41 @@ public class DFTTest extends TestCase {
         expectedRes[k++] = new Complex(-0.301436118898938,0.118492185600151);
         expectedRes[k++] = new Complex(-0.639028043502262,0.304593821408143);
         expectedRes[k++] = new Complex(-3.073479308812598,1.118520711871261);
-        expectedRes[k++] = new Complex(3.151505791271589,-0.594994616210640);
+        expectedRes[k] = new Complex(3.151505791271589,-0.594994616210640);
+
+        for(int j=0;j<10;j++)
+        {
+            Assert.assertTrue(areEqual(expectedRes[j].Re, res[j].Re));
+            Assert.assertTrue(areEqual(expectedRes[j].Im, res[j].Im));
+        }
+
+    }
+
+    public void testDFTofRamp()
+    {
+        double[] input = new double[10];
+        for(int i=0;i<input.length;i++)
+            input[i] = i;
+
+        Complex[] res = new Complex[10];
+        for(int i=0;i<10;i++)
+            res[i] = new Complex();
+
+        DFT.computeDFT(input, res);
+
+        Complex[] expectedRes = new Complex[10];
+        int k = 0;
+        expectedRes[k++] = new Complex(45.000000000000000,0.000000000000000);
+        expectedRes[k++] = new Complex(-5.000000000000000,15.388417685876266);
+        expectedRes[k++] = new Complex(-5.000000000000000,6.881909602355867);
+        expectedRes[k++] = new Complex(-5.000000000000000,3.632712640026803);
+        expectedRes[k++] = new Complex(-5.000000000000000,1.624598481164532);
+        expectedRes[k++] = new Complex(-5.000000000000000,0.000000000000000);
+        expectedRes[k++] = new Complex(-5.000000000000000,-1.624598481164532);
+        expectedRes[k++] = new Complex(-5.000000000000000,-3.632712640026803);
+        expectedRes[k++] = new Complex(-5.000000000000000,-6.881909602355867);
+        expectedRes[k] = new Complex(-5.000000000000000,-15.388417685876266);
+
 
         for(int j=0;j<10;j++)
         {
