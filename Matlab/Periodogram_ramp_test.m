a824a2394f7555bf1067a6a11bfd9a6a9551e16c
Andrea Luzzati <andrea.luzzati@gmail.com>
2015-07-05 20:15:28 +0200

    Create Periodogram_ramp_test.m

---
+++ Matlab/Periodogram_ramp_test.m
@@ -0,0 +1,8 @@
+a = zeros(1,10);
+for n = 1:10
+    a(n) = n-1;
+end
+ham = transpose(hamming(10));
+input = (a) .* ham;
+fourierTransform = fft( input );
+output = abs(fourierTransform) .* abs(fourierTransform) ./ 10;
