1a93f4e392e637b5aa1dbb1dad791fc2368183a8
Emanuele Canessa <ema0185@gmail.com>
2015-07-08 01:47:38 +0200

    WAVCreator: first release.

---
+++ app/src/main/java/it/unige/diten/dsp/speakerrecognition/WAVCreator.java
@@ -0,0 +1,221 @@
+package it.unige.diten.dsp.speakerrecognition;
+
+public class WAVCreator {
+    public byte[] data;
+    public String fileName;
+
+    private int chunkSize;
+    private int subChunk1Size;
+    private short audioFormat;
+    private short numChannels;
+    private int sampleRate;
+    private int byteRate;
+    private short blockAlign;
+    private short bitsPerSample;
+    private int subChunk2Size;
+
+    /**
+     * Instantiates an empty WAVE.
+     */
+    public WAVCreator() {
+        this.data = null;
+        this.fileName = null;
+
+        this.chunkSize = 0;
+        this.subChunk1Size = 0;
+        this.audioFormat = 0;
+        this.numChannels = 0;
+        this.sampleRate = 0;
+        this.byteRate = 0;
+        this.blockAlign = 0;
+        this.bitsPerSample = 0;
+        this.subChunk2Size = 0;
+    }
+
+    /**
+     * Create WAVE given fileName.
+     *
+     * @param fileName path to .wav file.
+     */
+    public WAVCreator(String fileName) {
+        this();
+        this.fileName = fileName;
+    }
+
+    /**
+     * Create WAVE given data samples.
+     *
+     * @param fileName    path to .wav file.
+     * @param data        samples array.
+     * @param sampleRate  sample rate in Hertz.
+     * @param numChannels numbers of channels.
+     */
+    public WAVCreator(String fileName, short[] data, int sampleRate, short numChannels) {
+        this(fileName);
+
+        this.data = toByteArray(data);
+        this.sampleRate = sampleRate;
+        this.numChannels = numChannels;
+    }
+
+    /**
+     * Read WAVE file from filePath.
+     *
+     * @return true if read successful, false otherwise.
+     */
+    public boolean read() {
+        boolean status = false;
+
+        if (null != fileName) {
+            // TODO Read audio from fileName.
+            status = true;
+        }
+
+        return status;
+    }
+
+    /**
+     * Write WAVE file to filepath.
+     *
+     * @return true if write successful, false otherwise.
+     */
+    public boolean write() {
+        boolean status = false;
+
+        if (null != fileName) {
+            // TODO Write audio to fileName.
+            status = true;
+        }
+
+        return status;
+    }
+
+    /**
+     * Convert short array into byte array for WAVE use.
+     * Format: LSB-MSB
+     *
+     * @param src Constant short[] input.
+     * @return byte[] array.
+     */
+    private byte[] toByteArray(final short[] src) {
+        byte[] retV = new byte[src.length * 2];
+
+        for (int i = 0; i < src.length; i++) {
+            retV[2 * i] = (byte) ((src[i]) & 0xFF); // LSB
+            retV[2 * i + 1] = (byte) ((src[i] >> 8) & 0xFF); // MSB
+        }
+
+        return retV;
+    }
+
+    /**
+     * Converts byte array to integer in LITTLE ENDIAN mode.
+     *
+     * @param src    byte array to be converted.
+     * @param offset 0-based offset in byte array.
+     * @return LITTLE ENDIAN representation of array.
+     */
+    private int byteArraytoInteger(final byte[] src, int offset) {
+        int retV = 0;
+
+        retV |= ((src[offset++]) & 0x000000FF); // LSB
+        retV |= ((src[offset++] << 8) & 0x0000FF00);
+        retV |= ((src[offset++] << 16) & 0x00FF0000);
+        retV |= ((src[offset] << 24) & 0xFF000000); // MSB
+
+        return retV;
+    }
+
+    /**
+     * Converts byte array to short in LITTLE ENDIAN mode.
+     *
+     * @param src    byte array to be converted.
+     * @param offset 0-based offset in byte array.
+     * @return LITTLE ENDIAN representation of array.
+     */
+    private short byteArrayToShort(final byte[] src, int offset) {
+        short retV = 0;
+
+        retV |= (short) ((src[offset++]) & 0x00FF); // LSB
+        retV |= (short) ((src[offset] << 8) & 0xFF00); // MSB
+
+        return retV;
+    }
+
+    /**
+     * Converts integer to byte array in LITTLE ENDIAN mode.
+     *
+     * @param src integer to be converted.
+     * @return byte array where first position is LSB and last position is MSB.
+     */
+    private byte[] integerToByteArray(final int src) {
+        byte[] retV = new byte[4];
+
+        retV[0] = (byte) ((src) & 0xFF); // LSB
+        retV[1] = (byte) ((src >> 8) & 0xFF);
+        retV[2] = (byte) ((src >> 16) & 0xFF);
+        retV[3] = (byte) ((src >> 24) & 0xFF); // MSB
+
+        return retV;
+    }
+
+    /**
+     * Converts short to byte array in LITTLE ENDIAN mode.
+     *
+     * @param src short to be converted.
+     * @return byte array where first position is LSB and last position is MSB.
+     */
+    private byte[] shortToByteArray(final short src) {
+        byte[] retV = new byte[2];
+
+        retV[0] = (byte) ((src) & 0xFF); // LSB
+        retV[1] = (byte) ((src >> 8) & 0xFF); // MSB
+
+        return retV;
+    }
+
+    /**
+     * Check and assign values from byte array.
+     *
+     * @param data RAW data.
+     * @return true if header was read correctly, false otherwise.
+     */
+    private boolean extractHeaderInfo(final byte[] data) {
+        boolean status = false;
+
+        if (data.length > 44) {
+            // Field check.
+            boolean riff_s = (0x46464952 == byteArraytoInteger(data, 0));
+            boolean wave_s = (0x45564157 == byteArraytoInteger(data, 8));
+            boolean fmt_s = (0x20746D66 == byteArraytoInteger(data, 12));
+            boolean data_s = (0x61746164 == byteArraytoInteger(data, 36));
+
+            chunkSize = byteArraytoInteger(data, 4);
+            subChunk1Size = byteArraytoInteger(data, 16);
+            audioFormat = byteArrayToShort(data, 20);
+            numChannels = byteArrayToShort(data, 22);
+            sampleRate = byteArraytoInteger(data, 24);
+            byteRate = byteArraytoInteger(data, 28);
+            blockAlign = byteArrayToShort(data, 32);
+            bitsPerSample = byteArrayToShort(data, 34);
+            subChunk2Size = byteArraytoInteger(data, 40);
+
+            status = (riff_s && wave_s && fmt_s && data_s);
+        }
+
+        return status;
+    }
+
+    /**
+     * Create byte array to be written to file using values.
+     *
+     * @return RAW data.
+     */
+    private byte[] insertHeaderInfo() {
+        byte[] retV = new byte[44];
+
+        // TODO Fill retV.
+
+        return retV;
+    }
+}
