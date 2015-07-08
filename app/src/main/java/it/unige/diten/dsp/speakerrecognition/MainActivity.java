d5170bf24b8d949be8153bce822cb2481d2c0ff5
Emanuele Canessa <ema0185@gmail.com>
2015-07-08 10:56:59 +0200

    MainActivity: added basic functions.

--- app/src/main/java/it/unige/diten/dsp/speakerrecognition/MainActivity.java
+++ app/src/main/java/it/unige/diten/dsp/speakerrecognition/MainActivity.java
@@ -3,6 +3,8 @@ package it.unige.diten.dsp.speakerrecognition;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -69,9 +71,32 @@ public class MainActivity extends Activity
                 if (null != fileName)
                 {
                     // TODO Start recording.
+                    // TODO Intent.
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private static String getCurrentDate()
