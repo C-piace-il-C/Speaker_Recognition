191bb89c3e93319a89c36ad6cdfa7b436633e6cb
Emanuele Canessa <ema0185@gmail.com>
2015-07-10 12:40:56 +0200

    SVM + SVMReceiver: base.

---
+++ app/src/main/java/it/unige/diten/dsp/speakerrecognition/SVM.java
@@ -0,0 +1,8 @@
+package it.unige.diten.dsp.speakerrecognition;
+
+
+public abstract class SVM
+{
+    public static final int roba = 0;
+    // TODO code SVM shit
+}
---
+++ app/src/main/java/it/unige/diten/dsp/speakerrecognition/SVMReceiver.java
@@ -0,0 +1,14 @@
+package it.unige.diten.dsp.speakerrecognition;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class SVMReceiver extends BroadcastReceiver
+{
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        // TODO start SVM shit
+    }
+}
