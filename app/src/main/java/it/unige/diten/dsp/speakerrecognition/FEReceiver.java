9f3a95752eb4ffee8d1b43c8a5f17dbd40e35e1b
Emanuele Canessa <ema0185@gmail.com>
2015-07-10 12:39:58 +0200

    Renamed FE_Receiver -> FEReceiver

--- app/src/main/java/it/unige/diten/dsp/speakerrecognition/FEReceiver.java
+++ app/src/main/java/it/unige/diten/dsp/speakerrecognition/FEReceiver.java
@@ -3,17 +3,15 @@ package it.unige.diten.dsp.speakerrecognition;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Environment;
 
-public class FE_Receiver extends BroadcastReceiver
+public class FEReceiver extends BroadcastReceiver
 {
     @Override
     public void onReceive(Context context, Intent intent)
     {
         // Start Feature Extractor to create FEATURE file.
         FeatureExtractor featureExtractor = new FeatureExtractor();
-        featureExtractor.execute(Environment.getExternalStorageDirectory() +
-                        "/" + MainActivity.PATH + "/" + MainActivity.fileName);
+        featureExtractor.execute(MainActivity.PATH + "/" + MainActivity.fileName);
     }
 
 
