3b8704240ed641ac8fde164914434f956b05cca4
Emanuele Canessa <ema0185@gmail.com>
2015-07-11 21:48:22 +0200

    Framer.readFromFile: use WAVCreator instead of WavIO. Framer.toDoubleArray: deleted (useless).

--- app/src/main/java/it/unige/diten/dsp/speakerrecognition/Framer.java
+++ app/src/main/java/it/unige/diten/dsp/speakerrecognition/Framer.java
@@ -30,50 +30,20 @@ public abstract class Framer
     public final static int FRAME_BYTE_SIZE = SAMPLES_IN_FRAME * BPS;
     /// Distance in Bytes between the beginning of a frame and the following
     public final static int FRAME_BYTE_SPACING = (int) ((float) FRAME_BYTE_SIZE * (1.0f - FRAME_OVERLAP_FACTOR));
+    /// Distance in Shorts between the beginning of a frame and the following
+    public final static int FRAME_SHORT_SPACING = FRAME_BYTE_SPACING / 2;
 
     /// Container for all frames
     private static Frame[] frames = null;
-
-    /**
-     * @brief   Converts a raw data source (array of bytes) to an array of double with zero-filling.
-     *          The raw data source is expected to contain integer elements.
-     *          by ALu. For info contact me at alu@cumallover.me.
-     *          note: yet to be debugged
-     * @param src Reference to the source.
-     * @param byteOffset Index of the first byte read from src.
-     * @param len        Number of elements to be read from src.
-     * @param byteStride Size in bytes of a single elment of src.
-     *                   note: byteStride must be in the range [1,8]
-     */
-    public static double[] toDoubleArray(byte[] src, int byteOffset, int len, int byteStride)
-    {
-        // byteStride must be even.
-        if ((byteStride & 0x1) != 0)
-            return null;
-
-        double[] retV = new double[len];
-
-        int i;
-
-        // Convert input to double.
-        for (i = 0; (i < len) && (byteOffset + i * byteStride + 1 < src.length); i++)
-        {
-            retV[i] =  (short) ((src[byteOffset + i * byteStride])          & 0x00FF); //LSB
-            retV[i] += (short) ((src[byteOffset + i * byteStride + 1] << 8) & 0xFF00); //MSB
-        }
-
-        // Zero-filling.
-        while (i < len)
-            retV[i++] = .0;
-
-        return (retV);
-    }
 
     /// Read WAVE file from SDCard
     public static void readFromFile(String fileName) throws Exception
     {
         Log.v (TAG, "Called readFromFile.");
-        WavIO readWAV = new WavIO(fileName);
+        //WavIO readWAV = new WavIO(fileName);
+        //readWAV.read();
+
+        WAVCreator readWAV = new WAVCreator(fileName);
         readWAV.read();
 
         if (readWAV.getSampleRate() != SAMPLE_RATE)
@@ -84,18 +54,31 @@ public abstract class Framer
         // Clear old data (garbage collector)
         frames = null;
 
-        int frameCount = readWAV.myData.length / FRAME_BYTE_SPACING;
+        short[] audioSamples = readWAV.getSamples();
+
+        int frameCount = audioSamples.length / FRAME_SHORT_SPACING;
         frames = new Frame[frameCount];
 
 
-        for (int C = 0; C < frameCount; C++) {
+        for (int C = 0; C < frameCount; C++)
+        {
             frames[C] = new Frame();
-            frames[C].data = toDoubleArray(
-                    readWAV.myData,         // src
-                    C * FRAME_BYTE_SPACING, // byte offset
-                    SAMPLES_IN_FRAME,       // len
-                    BPS                     // stride
-            );
+            frames[C].data = new double[SAMPLES_IN_FRAME];
+
+            int offset = C * FRAME_SHORT_SPACING;
+
+            int i;
+            // Copy samples to frame data.
+            for (i = 0; (i < SAMPLES_IN_FRAME) && (offset + i < audioSamples.length); i++)
+            {
+                frames[C].data[i] = audioSamples[offset + i];
+            }
+
+            // Zero-filling.
+            while (i < SAMPLES_IN_FRAME)
+            {
+                frames[C].data[i] = .0;
+            }
         }
 
         Log.v(TAG, "readFromFile: ended.");
