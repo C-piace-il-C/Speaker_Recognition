3836fc83d450ee6cc4cd21e7bfadca62d770b84e
Doddophonique <davide.oddone.21@gmail.com>
2015-08-07 21:41:36 +0200

    First draft of OverlapFactorDialog

---
+++ app/src/main/java/it/unige/diten/dsp/speakerrecognition/OverlapFactorDialog.java
@@ -0,0 +1,13 @@
+package it.unige.diten.dsp.speakerrecognition;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+public class OverlapFactorDialog extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
+}
