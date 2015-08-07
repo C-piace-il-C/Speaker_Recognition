a9b85b6245de174a444fd3fb102815a4c78de86b
Doddophonique <davide.oddone.21@gmail.com>
2015-08-07 18:34:22 +0200

    Refined CoefficientsDialog class

--- app/src/main/java/it/unige/diten/dsp/speakerrecognition/CoefficientsDialog.java
+++ app/src/main/java/it/unige/diten/dsp/speakerrecognition/CoefficientsDialog.java
@@ -1,11 +1,14 @@
 package it.unige.diten.dsp.speakerrecognition;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
@@ -16,12 +19,9 @@ import android.widget.Toast;
 
 import it.unige.diten.dsp.speakerrecognition.Structures.ModelingStructure;
 
-/**
- * Created by doddo on 8/6/15.
- */
 public class CoefficientsDialog extends DialogFragment{
 
-
+    private Activity activity;
     private MyView myView;
     private AlertDialog alertDialog;
     private AlertDialog.Builder builder;
@@ -29,46 +29,57 @@ public class CoefficientsDialog extends DialogFragment{
 
     final private String C      = "C_Coefficient";
     final private String Gamma  = "Gamma_Coefficient";
-    final private String cTitle = "C coefficient range";
-    final private String gTitle = "Gamma coefficient range";
+
+    private String cStartKey;
+    private String cEndKey;
+    private String cStepKey;
+    private String gStartKey;
+    private String gEndKey;
+    private String gStepKey;
 
     String[] defaultValues = new String[] {
             "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11",
             "12", "13", "14", "15", "-15", "-14", "-13", "-12", "-11",
-            "-10", "-9", "-8","-7", "-6", "-5", "-4", "-3", "-2", "-1",
+            "-10", "-9", "-8","-7", "-6", "-5", "-4", "-3", "-2", "-1"
     };
-
-
 
     final int minValue = 0;
     final int maxValue = defaultValues.length - 1;
 
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
+    private SharedPreferences settings;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final String tag = getTag();
 
-        builder = new AlertDialog.Builder(getActivity());
-        myView = new MyView(getActivity());
+        cStartKey   = getString(R.string.c_start_key);
+        cEndKey     = getString(R.string.c_end_key);
+        cStepKey    = getString(R.string.c_step_key);
+        gStartKey   = getString(R.string.g_start_key);
+        gEndKey     = getString(R.string.g_end_key);
+        gStepKey    = getString(R.string.g_step_key);
+
+        activity    = getActivity();
+        settings    = PreferenceManager.getDefaultSharedPreferences(activity);
+        builder     = new AlertDialog.Builder(activity);
+        myView      = new MyView(activity);
+
+        setPickers(tag);
 
         switch(tag)
         {
             case (C):
+                String cTitle = "C coefficient range";
                 init(cTitle, tag);
                 break;
             case (Gamma):
+                String gTitle = "Gamma coefficient range";
                 init(gTitle, tag);
                 break;
             default:
                 dismiss();
                 break;
         }
-
-        setPickers();
 
         return alertDialog;
     }
@@ -85,7 +96,8 @@ public class CoefficientsDialog extends DialogFragment{
 
         alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
             @Override
-            public void onShow(DialogInterface dialog) {
+            public void onShow(final DialogInterface dialog) {
+
                 Button button = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
 
                 button.setOnClickListener(new View.OnClickListener() {
@@ -94,46 +106,30 @@ public class CoefficientsDialog extends DialogFragment{
                         int start   = startPicker.getValue();
                         int end     = endPicker.getValue();
                         int step    = stepPicker.getValue();
+                        int middle  = defaultValues.length / 2;
 
-                        // The index is different from the actual value
-                        start = start   <= 16 ? start + 1   : start - maxValue - 1;
-                        end   = end     <= 16 ? end   + 1   : end   - maxValue - 1;
-                        step  = step    <= 16 ? step  + 1   : step  - maxValue - 1;
+                        // The index is different from the actual value, method valid only if the
+                        // array is antisymmetric
+                        start = start <= middle ? start + 1 : start - maxValue - 1;
+                        end   = end   <= middle ? end   + 1 : end   - maxValue - 1;
+                        step  = step  <= middle ? step  + 1 : step  - maxValue - 1;
 
+                        // If true, the user is trying to go from a positive value to a negative
+                        // value with a positive step or vice versa
                         if ((end - start) / step < 0)
-                            Toast.makeText(getActivity(),
-                                    "Step is in the wrong direction!", Toast.LENGTH_SHORT)
+                            Toast.makeText(activity, R.string.wrong_direction, Toast.LENGTH_SHORT)
                                     .show();
-
+                        // If true, start + (n * step) isn't equal to end given any value of n
                         else if ((end - start) % step != 0)
-                            Toast.makeText(getActivity(),
-                                    "Step inadequate for selected boundaries.", Toast.LENGTH_SHORT)
+                            Toast.makeText(activity, R.string.step_inadequate, Toast.LENGTH_SHORT)
                                     .show();
                         else if (end == start)
-                        {
-                            Toast.makeText(getActivity(),
-                                    "You can try a one-value cross validation in another preference screen.", Toast.LENGTH_SHORT)
+                            Toast.makeText(activity, R.string.one_value_validation,
+                                    Toast.LENGTH_SHORT)
                                     .show();
-
-                        }
                         else {
-                            switch(tag) {
-                                case(C): {
-                                    ModelingStructure.cStart = start;
-                                    ModelingStructure.cEnd = end;
-                                    ModelingStructure.cStep = step;
-                                    alertDialog.dismiss();
-                                    break;
-                                }
-                                case(Gamma):
-                                {
-                                    ModelingStructure.gStart = start;
-                                    ModelingStructure.gEnd = end;
-                                    ModelingStructure.gStep = step;
-                                    alertDialog.dismiss();
-                                    break;
-                                }
-                            }
+                            savePreferences(tag, start, end, step);
+                            alertDialog.dismiss();
                         }
                     }
                 });
@@ -141,7 +137,37 @@ public class CoefficientsDialog extends DialogFragment{
         });
     }
 
-    private void setPickers()
+    private void savePreferences(final String tag, int start, int end, int step) {
+
+        SharedPreferences.Editor editor = settings.edit();
+
+        switch (tag) {
+            case (C): {
+                ModelingStructure.cStart    = start;
+                ModelingStructure.cEnd      = end;
+                ModelingStructure.cStep     = step;
+
+                editor.putInt(cStartKey, start);
+                editor.putInt(cEndKey, end);
+                editor.putInt(cStepKey, step);
+                editor.apply();
+                break;
+            }
+            case (Gamma): {
+                ModelingStructure.gStart    = start;
+                ModelingStructure.gEnd      = end;
+                ModelingStructure.gStep     = step;
+
+                editor.putInt(gStartKey, start);
+                editor.putInt(gEndKey, end);
+                editor.putInt(gStepKey, step);
+                editor.apply();
+                break;
+            }
+        }
+    }
+
+    private void setPickers(final String tag)
     {
         // Initialize startPicker
         startPicker = (NumberPicker) myView.findViewById(R.id.cStartPicker);
@@ -160,8 +186,33 @@ public class CoefficientsDialog extends DialogFragment{
         stepPicker.setMinValue(minValue);
         stepPicker.setMaxValue(maxValue);
         stepPicker.setDisplayedValues(defaultValues);
-    }
 
+        int start, end, step;
+
+        switch (tag) {
+            // Check if the pickers were previously changed
+            case(C):
+                start = settings.getInt(cStartKey, 1);
+                end   = settings.getInt(cEndKey, 1);
+                step  = settings.getInt(cStepKey, 1);
+                break;
+            case(Gamma):
+                start = settings.getInt(gStartKey, 1);
+                end   = settings.getInt(gEndKey, 1);
+                step  = settings.getInt(gStepKey, 1);
+                break;
+            default:
+                start = 1;
+                end   = 1;
+                step  = 1;
+                break;
+        }
+        // The index is different from the actual value
+        startPicker.setValue(start  < 0 ? start + maxValue + 1 : start - 1);
+        endPicker.setValue  (end    < 0 ? end   + maxValue + 1 : end   - 1);
+        stepPicker.setValue (step   < 0 ? step  + maxValue + 1 : step  - 1);
+
+    }
 
     public void showDialog()
     {
