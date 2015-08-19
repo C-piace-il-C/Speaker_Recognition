0f5ab8dd6fbd89dee5f64b2674ccc3fbc3e89510
Emanuele Canessa <ema0185@gmail.com>
2015-08-19 17:48:07 +0200

    DCT: code review + comments.

--- app/src/main/java/it/unige/diten/dsp/speakerrecognition/DCT.java
+++ app/src/main/java/it/unige/diten/dsp/speakerrecognition/DCT.java
@@ -1,35 +1,38 @@
 package it.unige.diten.dsp.speakerrecognition;
 
 /**
+ * DCT.
  * Type-II DCT.
  */
 public abstract class DCT
 {
     /**
-     * @brief       Computes the type-II DCT on a real sequence src.
-     *              The result is a new real sequence of the same length as src.
+     * Computes the type-II DCT on a real sequence src.
+     * The result is a new real sequence of the same length as src.
      * @param src   The source sequence.
      * @param len   The number of samples of the DCT to compute.
-     *              This will also be the length of the returned sequence.
-     *              Default value is src.length.
-     * @return      double array containing the DCT.
+     * @return      Double array containing the DCT.
      */
     public static double[] computeDCT (double[] src, int len)
     {
-        final double N = src.length;
-        double[] DCT = new double[len];
+        double   N      = src.length;
+        double   adj    = Math.sqrt(2.0 / N);
+        double   stride = Math.PI / N;
 
-        double adj = Math.sqrt(2.0 / N);
+        double[] DCT    = new double[len];
 
         // Compute only the first len values
-        for (int k = 0; k < len; k++)
+        for (int i = 0; i < len; i++)
         {
-            DCT[k] = .0;
+            DCT[i] = .0;
+
             for (int n = 0; n < N; n++)
             {
-                DCT[k] += src[n] * Math.cos(Math.PI / N * (n + 0.5) * k);
+                DCT[i] += src[n] * Math.cos(stride * (n + 0.5) * i);
             }
-            DCT[k] *= adj;
+
+            // Scale coefficients.
+            DCT[i] *= adj;
         }
 
         DCT[0] /= Math.sqrt(2.0);
@@ -37,6 +40,12 @@ public abstract class DCT
         return (DCT);
     }
 
+    /**
+     * Computes the type-II DCT on a real sequence src.
+     * The result is a new real sequence of the same length as src.
+     * @param src   The source sequence.
+     * @return      Double array containing the DCT.
+     */
     public static double[] computeDCT(double[] src)
     {
         return computeDCT(src, src.length);
