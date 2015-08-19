c651d9d3cdc382860caaf6f74a14edccc543797e
Emanuele Canessa <ema0185@gmail.com>
2015-08-19 17:38:39 +0200

    Complex: code review + comments.

--- app/src/main/java/it/unige/diten/dsp/speakerrecognition/Complex.java
+++ app/src/main/java/it/unige/diten/dsp/speakerrecognition/Complex.java
@@ -1,75 +1,129 @@
-// Completely tested
-
 package it.unige.diten.dsp.speakerrecognition;
 
 /**
- * Complex
- * Class to handle complex numbers
+ * Complex.
+ * Class to handle complex numbers.
  */
-public class Complex {
+public class Complex
+{
     public double Re;
     public double Im;
 
-    public Complex() {
+    /**
+     * Create a new complex (0 + i0).
+     */
+    public Complex()
+    {
         Re = .0;
         Im = .0;
     }
 
-    public Complex(double re, double im)
+    /**
+     * Create a complex given Real and Imaginary part.
+     * @param Re    Real part.
+     * @param Im    Imaginary part.
+     */
+    public Complex(double Re, double Im)
     {
-        Re = re;
-        Im = im;
+        this.Re = Re;
+        this.Im = Im;
     }
 
+    /**
+     * Get vector length.
+     * @return  Length.
+     */
     public double getLength()
     {
         return (Math.sqrt(Re * Re + Im * Im));
     }
+
+    /**
+     * Get vector square length.
+     * @return  Squared length.
+     */
     public double getSquareLength()
     {
         return (Re * Re + Im * Im);
     }
+
+    /**
+     * Get vector phase.
+     * @return  Phase.
+     */
     public double getPhase()
     {
         return (Math.atan2(Im, Re));
     }
 
-    public void addPhase( double relPhase )
+    /**
+     * Add vector phase to existing.
+     * @param relPhase  Phase in radiant.
+     */
+    public void addPhase(double relPhase)
     {
         double len = getLength();
-        double angle = getPhase()+relPhase;
+        double angle = getPhase() + relPhase;
+
         Re = len * Math.cos(angle);
         Im = len * Math.sin(angle);
     }
-    // return a new Complex object whose value is (this + b)
-    public Complex plus(Complex b) {
-        Complex a = this;             // invoking object
-        double real = a.Re + b.Re;
-        double imag = a.Im + b.Im;
-        return new Complex(real, imag);
+
+    /**
+     * Sum two Complex without modify objects (this + b).
+     * @param b Complex to sum.
+     * @return  Sum.
+     */
+    public Complex plus(Complex b)
+    {
+        Complex a = this;
+        double re = a.Re + b.Re;
+        double im = a.Im + b.Im;
+        return new Complex(re, im);
     }
-    // return a new Complex object whose value is (this - b)
+
+    /**
+     * Subtract two Complex without modify objects (this - b).
+     * @param b Complex to subtract.
+     * @return  Subtract.
+     */
     public Complex minus(Complex b)
     {
         Complex a = this;
-        double real = a.Re - b.Re;
-        double imag = a.Im - b.Im;
-        return new Complex(real, imag);
-    }
-    // return a new Complex object whose value is (this * b)
-    public Complex times(Complex b) {
-        Complex a = this;
-        double real = a.Re * b.Re - a.Im * b.Im;
-        double imag = a.Re * b.Im + a.Im * b.Re;
-        return new Complex(real, imag);
+        double re = a.Re - b.Re;
+        double im = a.Im - b.Im;
+        return new Complex(re, im);
     }
 
-    // scalar multiplication
-    // return a new object whose value is (this * alpha)
-    public Complex times(double alpha) {
+    /**
+     * Multiply two Complex without modify objects (this * b).
+     * @param b Complex to multiply.
+     * @return  Product.
+     */
+    public Complex times(Complex b)
+    {
+        Complex a = this;
+        double re = (a.Re * b.Re) - (a.Im * b.Im);
+        double im = (a.Re * b.Im) + (a.Im * b.Re);
+        return new Complex(re, im);
+    }
+
+    /**
+     * Scalar multiplication without modify objects (this * alpha).
+     * @param alpha Scalar to multiply.
+     * @return      Product.
+     */
+    public Complex times(double alpha)
+    {
         return new Complex(alpha * Re, alpha * Im);
     }
 
-    // return a new Complex object whose value is the conjugate of this
-    public Complex conjugate() {  return new Complex(Re, -Im); }
+    /**
+     * Conjugate Complex without modify objects.
+     * @return Conjugate.
+     */
+    public Complex conjugate()
+    {
+        return new Complex(Re, -Im);
+    }
 }
